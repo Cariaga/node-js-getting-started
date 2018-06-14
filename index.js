6ae429285e27bb88babc6dfcc5b57749f694f133
Kaivaan <cariaga.info@gmail.com>
2018-06-14 22:02:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.send('hello'))
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+//.get('/', (req, res) => res.send('pages/index'))
