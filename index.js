e59206333f12e38f7b55b33a62c034bd54853284
Kaivaan <cariaga.info@gmail.com>
2018-06-14 22:22:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const PokerHand = require('poker-hand-evaluator');
+const myPokerHand = new PokerHand('KS KH QC AH AD');
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send('hello'))
+  .get('/', (req, res) => res.send(myPokerHand.describe()))
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
